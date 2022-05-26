08a5697ff9676b40bba98138764fb57a0bbcc1ec
jluctamar <jeffly.luctamar@gmail.com>
2022-05-26 14:10:09 -0400

    updates for deplyment setup

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -177,6 +177,10 @@ const Wrapper = styled.div`
     background: rgb(255 255 255 / 10%);
     backdrop-filter: blur(8px);
     box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;
+
+    .meal-types {
+      width: 100%;
+    }
   
     form {
       align-self: flex-end;
